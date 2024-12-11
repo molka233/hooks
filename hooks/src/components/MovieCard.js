6dfb5ff08bcec852276eb2cd78c0ae19e90c5d8b
molka233 <molkabouchaala@gmail.com>
2024-12-11 19:17:37 +0100

    adding hooks

--- hooks/src/components/MovieCard.js
+++ hooks/src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import movieRating from '../components/Rating';
+import Rating from '../components/Rating';
 function MovieCard({movie}) {
   return (
     <div>
@@ -12,7 +12,7 @@ function MovieCard({movie}) {
         <Card.Text>{movie.description}
           
         </Card.Text>
-       <movieRating movieRating={movie.rate} isMovieRating={true}/>
+       <Rating movieRating={movie.rate} isMovieRating={true}/>
       </Card.Body>
     </Card>
     </div>
